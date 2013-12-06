6cb1b042fd98cb43d7d03b824402cbc67ee07f1f
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2013-12-07 08:53:35 +0900

    "aws" is a standalone command

--- update.py
+++ update.py
@@ -6,8 +6,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 subprocess.call([ 'python',
                   os.path.join('..', 'venus', 'planet.py'),
                   'planet.ini' ])
-subprocess.call([ 'python',
-                  'aws', 's3', 'sync',
+subprocess.call([ 'aws', 's3', 'sync',
                   '--region', 'us-east-1',
                   '--acl', 'public-read',
                   'public/', 's3://tempura.8-p.info/' ])
